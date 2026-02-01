3f7a0518854a99466970f36ef6006e8650e9908d
OmniDAI <daiomni9@gmail.com>
2026-02-01 20:22:18 +0000

    C Syntax

---
+++ main.c
@@ -0,0 +1,16 @@
+// This is a comment in c
+
+// First program in c
+
+#include <stdio.h>
+// this is a preprocessor command whch tells the compiler to incude the contents of studio.h file in the program
+
+int main() {
+  // this is the entry point of the program
+  
+  printf("Hello world");
+  // the printf funtion is used to print the output to the screen
+  return 0;
+  // this is the program's way of telling te operating system that i am done now everything is fine.
+  
+}
